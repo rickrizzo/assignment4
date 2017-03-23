e8e2a03678f55816e119f0ef2138083073d7fe33
Robert Russo <rrusso44@gmail.com>
2017-03-22 22:09:12 -0400

    File Open/Close

--- assignment4.c
+++ assignment4.c
@@ -2,8 +2,30 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int main(int argc, char** argv) {
+#define FILESIZE 16
 
-  printf("HELLO WORLD!\n");
+int main(int argc, char **argv) {
 
+  // MPI Variables
+  int mpi_rank, mpi_size;
+  MPI_Status status;
+  MPI_File file;
+  MPI_Datatype filetype;
+
+  // Initialize MPI
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
+  // Buffer Allocation
+  int *buffer = (int *) malloc(FILESIZE / mpi_size);
+
+  // Access File
+  MPI_File_open(MPI_COMM_WORLD, "test.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
+  MPI_File_close(&file);
+
+  // Close MPI
+  free(buffer);
+  MPI_Finalize();
+  exit(EXIT_SUCCESS);
 }

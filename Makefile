b1dd94f05f10501f873de49a2ff2aa4cdbce038b
Kiana McNellis <kiana@kmcnellis.com>
2017-03-23 15:54:22 -0400

    Updates to makefile and added timer stub

--- Makefile
+++ Makefile
@@ -1,2 +1,17 @@
-all:
-	mpicc assignment4.c -o assignment4.out
+.PHONY: compile run delete clean
+
+all: compile run delete clean
+
+test: compile run
+	
+compile:
+	mpicc assignment4.c timestub.c -o assignment4.out
+
+clean:
+	rm -f assignment4.out
+
+delete:
+	rm -f text.txt
+
+run:
+	mpirun -n 1 ./assignment4.out
--- assignment4.c
+++ assignment4.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-
+#include "timestub.h"
+// #include <hwi/include/bqc/A2_inlines.h>
 #define FILESIZE 1000000
 
 int main(int argc, char **argv) {
@@ -12,6 +13,10 @@ int main(int argc, char **argv) {
   MPI_File file;
   MPI_Datatype filetype;
   MPI_Offset offset;
+
+  long long start_cycle_time=0;
+  long long end_cycle_time=0;
+  long long total_cycle_time=0, total_cycle_time_comb=0;
 
   // Initialize MPI
   MPI_Init(&argc, &argv);
@@ -26,17 +31,28 @@ int main(int argc, char **argv) {
   }
 
   // Access File
-  // |MPI_MODE_DELETE_ON_CLOSE
   MPI_File_open(MPI_COMM_WORLD, "test.txt", MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
 
   // Set Offset
   offset = mpi_rank * (FILESIZE / mpi_size * sizeof(int));
 
+  start_cycle_time = GetTimeBase();
   // Write File
   MPI_File_write_at_all(file, offset, buffer, FILESIZE / mpi_size, MPI_INT, &status);
 
   // Read File
-  MPI_File_write_at_all(file, offset, buffer, FILESIZE / mpi_size, MPI_INT, &status);
+  // MPI_File_write_at_all(file, offset, buffer, FILESIZE / mpi_size, MPI_INT, &status);
+  //
+  end_cycle_time = GetTimeBase();
+  total_cycle_time = end_cycle_time - start_cycle_time;
+
+  MPI_Allreduce(
+      &total_cycle_time,
+      &total_cycle_time_comb,
+      1,
+      MPI_LONG_LONG_INT,
+      MPI_MAX,
+      MPI_COMM_WORLD);
 
   // Close File
   MPI_Barrier(MPI_COMM_WORLD);
